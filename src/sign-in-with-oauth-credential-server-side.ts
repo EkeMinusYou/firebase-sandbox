4bb2edfca3a2aed9e1d9ecbf7332d9ed33aa04c2
EkeMinusYou <euonymuslke@gmail.com>
2023-01-12 16:56:52 +0900

    convert args to obj

--- src/sign-in-with-oauth-credential-server-side.ts
+++ src/sign-in-with-oauth-credential-server-side.ts
@@ -3,20 +3,20 @@
 const API_KEY = process.env.API_KEY ?? "";
 const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=${API_KEY}`;
 
-export async function signInWithCredential(
-  idToken: string,
-  providerId: string,
-  requestUri: string
-) {
+export async function signInWithCredential(params: {
+  idToken: string;
+  providerId: string;
+  requestUri: string;
+}) {
   const response = await fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      postBody: `id_token=${idToken}&providerId=${providerId}`,
+      postBody: `id_token=${params.idToken}&providerId=${params.providerId}`,
       returnSecureToken: true,
-      requestUri,
+      requestUri: params.requestUri,
     }),
   });
   console.log(await response.json());
