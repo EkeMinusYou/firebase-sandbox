7f608a58daf40e580c4ebb4d8e922623b1fa231b
EkeMinusYou <euonymuslke@gmail.com>
2023-01-12 15:12:34 +0900

    rename

--- src/sign-in-with-oauth-credential-server-side.ts
+++ src/sign-in-with-oauth-credential-server-side.ts
@@ -1,7 +1,7 @@
 // See https://cloud.google.com/identity-platform/docs/use-rest-api#section-sign-in-with-oauth-credential
 
-const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY ?? "";
-const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=${FIREBASE_API_KEY}`;
+const API_KEY = process.env.API_KEY ?? "";
+const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key=${API_KEY}`;
 
 export async function signInWithCredential(
   idToken: string,
